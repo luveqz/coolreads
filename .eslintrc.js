c29e903eaac45daa361ddc9b4641e478467468c4
Luis Velásquez <luvejo.dev@gmail.com>
2023-03-31 23:19:14 -0500

    style: add Nuxt's ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    '@nuxtjs/eslint-config-typescript',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
   ],
