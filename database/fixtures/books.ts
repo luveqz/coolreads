8887d93755d3bfa4c828d3722de236128dcce942
Luis Velásquez <luvejo.dev@gmail.com>
2023-04-03 19:30:01 -0500

    refactor: move book fixtures to its own separate file

---
+++ database/fixtures/books.ts
@@ -0,0 +1,42 @@
+export const books = [
+  {
+    id: '1',
+    title: 'Last Evening on Earth',
+    authors: ['Roberto Bolaño'],
+    summary: '',
+    coverUrl:
+      'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Fleoe.webp?alt=media',
+  },
+  {
+    id: '2',
+    title: 'Crónica del pájaro que da cuerda al mundo',
+    authors: ['Haruki Murakami'],
+    summary: '',
+    coverUrl:
+      'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Fcdpqdcam.webp?alt=media',
+  },
+  {
+    id: '3',
+    title: 'A Confederacy of Dunces',
+    authors: ['John Kennedy Toole'],
+    summary: '',
+    coverUrl:
+      'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Facod.webp?alt=media',
+  },
+  {
+    id: '4',
+    title: 'No Longer Human',
+    authors: ['Osamu Dazai'],
+    summary: '',
+    coverUrl:
+      'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Fnlh.webp?alt=media',
+  },
+  {
+    id: '5',
+    title: 'The Catcher in the Rye ',
+    authors: ['J.D. Salinger'],
+    summary: '',
+    coverUrl:
+      'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Ftcitr.webp?alt=media',
+  },
+]
--- database/fixtures/recent-activity.ts
+++ database/fixtures/recent-activity.ts
@@ -1,4 +1,5 @@
 import user from './user'
+import { books } from './books'
 
 export const review = {
   user,
@@ -39,43 +40,7 @@ export const quote = {
 
 export const list = {
   user,
-  books: [
-    {
-      id: '1',
-      title: 'Last Evening on Earth',
-      authors: ['Roberto Bolaño'],
-      coverUrl:
-        'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Fleoe.webp?alt=media',
-    },
-    {
-      id: '2',
-      title: 'Crónica del pájaro que da cuerda al mundo',
-      authors: ['Haruki Murakami'],
-      coverUrl:
-        'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Fcdpqdcam.webp?alt=media',
-    },
-    {
-      id: '3',
-      title: 'A Confederacy of Dunces',
-      authors: ['John Kennedy Toole'],
-      coverUrl:
-        'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Facod.webp?alt=media',
-    },
-    {
-      id: '4',
-      title: 'No Longer Human',
-      authors: ['Osamu Dazai'],
-      coverUrl:
-        'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Fnlh.webp?alt=media',
-    },
-    {
-      id: '5',
-      title: 'The Catcher in the Rye ',
-      authors: ['J.D. Salinger'],
-      coverUrl:
-        'https://firebasestorage.googleapis.com/v0/b/fictolab.appspot.com/o/coolreads%2Fcovers%2Ftcitr.webp?alt=media',
-    },
-  ],
+  books,
   name: 'Anti-hero’s journey',
   meta: {
     publishedAt: '2w',
