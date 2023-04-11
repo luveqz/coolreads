9cd4f5bf7180b967b3d4356fd3ed89eb13da2b71
Luis Velásquez <luvejo.dev@gmail.com>
2023-04-11 15:42:37 -0500

    fix: prevent event arguments to be passed to the event handler

--- components/ActivityTabs.vue
+++ components/ActivityTabs.vue
@@ -37,7 +37,7 @@ const onClick = (activityType?: ActivityType) => {
         <BaseTab
           label="Recent Activity"
           :is-active="!activeType"
-          @click="onClick"
+          @click="onClick()"
         />
         <BaseTab
           label="Reviews"
